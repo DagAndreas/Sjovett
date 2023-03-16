3c925ecb58010191adbf34486b7074694383d4c0
Dag Andreas <dafolvel@ifi.uio.no>
2023-03-16 14:01:32 +0100

    test 4

--- app/src/main/java/com/example/gruppe_16/MainActivity.kt
+++ app/src/main/java/com/example/gruppe_16/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("dag 123
+                    Greeting("dag 123")
                 }
             }
         }
