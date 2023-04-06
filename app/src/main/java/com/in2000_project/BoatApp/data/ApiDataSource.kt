189d4137c1a5b078e732470228496e6214b8d79d
Truls <trulsvidnes@hotmail.no>
2023-04-06 15:01:19 +0200

    Tried to add API key to request headers

--- app/src/main/java/com/in2000_project/BoatApp/data/ApiDataSource.kt
+++ app/src/main/java/com/in2000_project/BoatApp/data/ApiDataSource.kt
@@ -8,10 +8,14 @@ import com.in2000_project.BoatApp.R
 
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 
 import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.serialization.gson.*
+import java.lang.System.exit
+import kotlin.system.exitProcess
 
 
 class ApiDataSource () {
@@ -21,28 +25,80 @@ class ApiDataSource () {
         }
     }
 
-    suspend fun fetchLocationForecastData(path: String): LocationForecastResponse {
+    suspend fun fetchLocationForecastData(itempath: String): LocationForecastResponse {
         Log.d("API_request", "attempting fetchLocationForecastData.launch")
-        val response = client.get(path){
-            headers{
-                append(name = "X-Gravitee-Api-Key", value = "dc1732ae-a8a0-4dd5-8052-26094bfbca11")
-            }
-        }.body<LocationForecastResponse>()
-        Log.d("API_request", "fetchLocationForecastData.launch success, response: ")
-        Log.i("API_request", response.toString())
+        val response = try {
+            client.get(itempath) {
+                url(itempath)
+                headers {
+                    append(
+                        name = "X-Gravitee-Api-Key",
+                        value = "dc1732ae-a8a0-4dd5-8052-26094bfbca11"
+                    )
+                }
+            }.body<LocationForecastResponse>()
+            /*
+            headers {
+                        append(
+                            name = "X-Gravitee-Api-Key",
+                            value = "dc1732ae-a8a0-4dd5-8052-26094bfbca11"
+                        )
+                    }
+             */
+        }  catch (e: RedirectResponseException) {
+            // 3xx - responses
+            Log.e("API_request 3xx", e.response.status.description)
+            exitProcess(0)
+        } catch (e: RedirectResponseException) {
+            // 4xx - responses
+            Log.e("API_request 4xx", e.response.status.description)
+            exitProcess(0)
+        } catch (e: ServerResponseException) {
+            // 5xx - responses
+            Log.e("API_request 5xx", e.response.status.description)
+            exitProcess(0)
+        } catch (e: Exception) {
+            // General exception
+            Log.e("API_request xxx", itempath)
+            Log.e("API_request xxx", e.message.toString())
+            exitProcess(0)
+        }
+        Log.d("API_request", "fetchLocationForecastData.launch success, response: ${response}")
         return response
     }
 
 
-    suspend fun fetchMetAlertsData(path: String): MetAlertsResponse {
+    suspend fun fetchMetAlertsData(url: String): MetAlertsResponse {
         Log.d("API_request", "attempting fetchMetAlertsData.launch")
-        val response = client.get(path){
-            headers{
-                append(name = "X-Gravitee-Api-Key", value = "dc1732ae-a8a0-4dd5-8052-26094bfbca11")
-            }
-        }.body<MetAlertsResponse>()
-        Log.d("API_request", "fetchMetAlertsData.launch success, response: ")
-        Log.i("API_request", response.toString())
+        val response = try {
+            client.get(url) {
+                url(url)
+                headers {
+                    append(
+                        name = "X-Gravitee-Api-Key",
+                        value = "dc1732ae-a8a0-4dd5-8052-26094bfbca11"
+                    )
+                }
+            }.body<MetAlertsResponse>()
+        }  catch (e: RedirectResponseException) {
+            // 3xx - responses
+            Log.e("API_request 3xx", e.response.status.description)
+            exitProcess(0)
+        } catch (e: RedirectResponseException) {
+            // 4xx - responses
+            Log.e("API_request 4xx", e.response.status.description)
+            exitProcess(0)
+        } catch (e: ServerResponseException) {
+            // 5xx - responses
+            Log.e("API_request 5xx", e.response.status.description)
+            exitProcess(0)
+        } catch (e: Exception) {
+            // General exception
+            Log.e("API_request xxx", url)
+            Log.e("API_request xxx", e.message.toString())
+            exitProcess(0)
+        }
+        Log.d("API_request", "fetchMetAlertsData.launch success, response: ${response}")
         return response
     }
 
