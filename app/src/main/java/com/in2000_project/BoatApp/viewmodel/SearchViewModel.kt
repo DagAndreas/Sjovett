8f3a6e4a2e45501459e0dbf4f06b7119b4d01120
theresml <theresml@ifi.uio.no>
2023-04-17 11:22:56 +0200

    Added norwegian cities to SearchViewModel

--- app/src/main/java/com/in2000_project/BoatApp/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/in2000_project/BoatApp/viewmodel/SearchViewModel.kt
@@ -1,6 +1,52 @@
 package com.in2000_project.BoatApp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.in2000_project.BoatApp.model.geoCode.CityName
+import kotlinx.coroutines.flow.*
 
 class SearchViewModel: ViewModel() {
+    private val _locationSearch = MutableStateFlow("")
+    val locationSearch = _locationSearch.asStateFlow()
+
+    private val _searchInProgress = MutableStateFlow(false)
+    val searchInProgress = _searchInProgress.asStateFlow()
+
+    private val _cities = MutableStateFlow(listOf<CityName>())
+    val cities = locationSearch
+        .combine(_cities){ text, cities ->
+            if(text.isBlank()){
+                cities //viser alle steder om man ikke har begynt søk
+            }else{
+                cities.filter{
+                    it.matchesSearch(text)
+                }
+            }
+        }
+        .stateIn( //for å få stateFlow
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            _cities.value
+        )
+
+
+    fun onSearchChange(text: String){
+        _locationSearch.value = text
+    }
+}
+
+
+
+val norwegianCities = listOf(
+    "Oslo",
+    "Trondheim",
+    "Bergen"
+)
+
+fun getAllCities(cities: List<String>): MutableList<CityName>{
+    val listOfCities = emptyList<CityName>().toMutableList()
+    for(city in cities){
+        listOfCities.add(CityName(city, "Norway"))
+    }
+    return listOfCities
 }
