fbd78208480c6446f15f30c5dbbe0b307c04da00
trulsbv <trulsbv@ifi.uio.no>
2023-04-30 23:41:45 +0200

    Cleanup

--- app/src/main/java/com/in2000_project/BoatApp/viewmodel/AlertsMapViewModel.kt
+++ app/src/main/java/com/in2000_project/BoatApp/viewmodel/AlertsMapViewModel.kt
@@ -1,24 +1,20 @@
 package com.in2000_project.BoatApp.viewmodel
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.in2000_project.BoatApp.maps.*
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolygonOptions
 import com.in2000_project.BoatApp.ZoneClusterItem
 import com.in2000_project.BoatApp.data.MapStateCluster
 import com.in2000_project.BoatApp.ZoneClusterManager
 import com.in2000_project.BoatApp.data.AlertsMapUiState
-import com.in2000_project.BoatApp.data.StormWarningUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -29,25 +25,19 @@ import javax.inject.Inject
 @HiltViewModel
 class AlertsMapViewModel @Inject constructor(): ViewModel() {
     private val listOfClusters = mutableListOf<ZoneClusterItem>()
-
     private val _alertsMapUiState = MutableStateFlow(AlertsMapUiState())
     val alertsMapUiState = _alertsMapUiState.asStateFlow()
-
     private val _state = MutableStateFlow(
         MapStateCluster(
             lastKnownLocation = null,
             clusterItems = listOfClusters
         )
     )
-
-    //InfoKort
     var stormvarselInfoPopUp by mutableStateOf(true)
-
     val state: StateFlow<MapStateCluster> = _state.asStateFlow()
 
     fun updateUserLocation(lat: Double, lng: Double) {
         _alertsMapUiState.update {
-            // setter warningList til å være en MetAlertsResponse
             (it.copy(longitude = lng, latitude = lat))
         }
     }
@@ -65,7 +55,6 @@ class AlertsMapViewModel @Inject constructor(): ViewModel() {
                 clusterItems = listOfClusters
             )
         }
-        //Oppdatere state?
     }
 
     fun resetCluster() {
@@ -92,11 +81,9 @@ class AlertsMapViewModel @Inject constructor(): ViewModel() {
                 }
             }
         } catch (e: SecurityException) {
-            // Show error or something
+            Log.e("SecurityException", e.toString())
         }
     }
-
-
 
     fun setupClusterManager(
         context: Context,
@@ -106,13 +93,13 @@ class AlertsMapViewModel @Inject constructor(): ViewModel() {
         clusterManager.addItems(state.value.clusterItems)
         return clusterManager
     }
-
-    fun calculateZoneLatLngBounds(): LatLngBounds {
+/*
+    fun calculateZoneLatLngBounds(): LatLngBounds { //TODO: This function zooms in on all storms on the map
         // Get all the points from all the polygons and calculate the camera view that will show them all.
         val latLngs = state.value.clusterItems.map { it.polygonOptions }
             .map { it.points.map { LatLng(it.latitude, it.longitude) } }.flatten()
         return latLngs.calculateCameraViewPoints().getCenterOfPolygon()
     }
 
-
+ */
 }
